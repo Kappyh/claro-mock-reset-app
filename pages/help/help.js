d42dacf63874292a3f844692c42bd2365a4384ae
Gabriela Mendes <contato@gabrielamendes.dev>
2022-01-30 20:03:39 -0300

    About page, footer and header
    - Adiciona conteúdo da página About
    - Remove links do js da index e insere no html
    - Adiciona footer e header para as páginas about e help
    - Insere link no header das páginas about e help que redirecionam para a home.

---
+++ pages/help/help.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // var elems = document.querySelectorAll('.collapsible');
+    // var instances = M.Collapsible.init(elems, options);
+    console.log('teste');
+});
+
+document.addEventListener('load', ()=>{
+    console.log('hello')
+})
--- render.js
+++ render.js
@@ -1,6 +1,5 @@
 const QAReset = document.querySelector('#QA-reset');
 const OthersReset =  document.querySelector('#Others-reset');
-const links = document.querySelectorAll('a');
 
 QAReset.addEventListener('click', ()=>{
     runUpdateQA();
@@ -8,8 +7,4 @@ QAReset.addEventListener('click', ()=>{
 
 OthersReset.addEventListener('click', ()=>{
     runtUpdateOthers();
-});
-
-links[0].addEventListener('click', ()=>{
-   window.location.href = './pages/about/about.html';
 });
